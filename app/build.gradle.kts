57f6eb80d68c501c2877bd22b7f67836e7c06312
MangoLambda <75704869+MangoLambda@users.noreply.github.com>
2026-07-19 23:25:54 +0000

    Bump version to 1.1.35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "com.example.flightlog"
         minSdk = 29
         targetSdk = 36
-        versionCode = 42
-        versionName = "1.1.34"
+        versionCode = 43
+        versionName = "1.1.35"
 
         buildConfigField("String", "THUNDERFOREST_API_KEY", "\"$thunderforestApiKey\"")
         buildConfigField("String", "FLIGHTLOG_UPDATE_BASE_URL", "\"$flightLogUpdateBaseUrl\"")
